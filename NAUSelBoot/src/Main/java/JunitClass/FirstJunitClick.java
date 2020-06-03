3f30b6147b8d19dc28c8f773c3831f5c6b5cc4cc
Karatay-Inci <ikaratay@na.edu>
2020-06-03 11:05:19 -0500

    June 3

---
+++ NAUSelBoot/src/Main/java/JunitClass/FirstJunitClick.java
@@ -0,0 +1,55 @@
+package Main.java.JunitClass;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstJunitClick {
+
+    static WebDriver driver;
+    static String baseURL;
+
+    @BeforeAll
+    public static void setUp(){
+        baseURL="https://letskodeit.teachable.com/";
+        System.setProperty("webdriver.chrome.driver","/Users/incikaratay/Desktop/SELENIUM/chromedriver");
+        driver =  new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(baseURL);
+        System.out.println("@BeforeAll - initiated and navigated to web page ");
+    }
+
+    @AfterAll
+    public  static void tearDown(){
+        driver.quit();
+        System.out.println("@AfterAll - closed browser session");
+    }
+
+    @BeforeEach
+    public void init(){
+        driver.navigate().refresh();
+        System.out.println("@BeforeEach - refresh the page");
+    }
+
+    @AfterEach
+    public void done(){
+        driver.navigate().refresh();
+        System.out.println("@BeforeAll - refresh the page");
+    }
+
+    @Test
+    public void test1(){
+        driver.findElement(By.cssSelector("a.navbar-brand.header-logo")).click();
+        System.out.println("@Test1 - clicking directly");
+    }
+
+    @Test
+    public void test2() throws InterruptedException {
+        Thread.sleep(5000);
+        WebElement logo= driver.findElement(By.cssSelector("a.navbar-brand.header-logo"));
+        logo.click();
+        System.out.println("@Test2 - clicking WebElement logo");
+    }
+}
